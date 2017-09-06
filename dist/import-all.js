f0326661b331091e8a2f348fe6ae206cdafc5d62
mk-pmb <mk-pmb@users.noreply.github.com>
2017-09-06 22:44:04 +0200

    all-in-one wrapper

---
+++ dist/import-all.js
@@ -0,0 +1,20 @@
+﻿/*jslint indent: 2, maxlen: 80, node: true */
+/* -*- tab-width: 2 -*- */
+/*global define: true */
+((typeof define === 'function') && define.amd ? define : function (factory) {
+  'use strict';
+  var m = ((typeof module === 'object') && module), e = (m && m.exports);
+  if (e) { m.exports = (factory(require, e, m) || m.exports); }
+})(function req() {
+  'use strict';
+  return {
+    diatonic:     require('./diatonic.json'),
+    drums:        require('./drums.json'),
+    duration:     require('./duration.json'),
+    instruments:  require('./instruments.json'),
+    interval:     require('./interval.json'),
+    key:          require('./key.json'),
+    pitch:        require('./pitch.json'),
+    scale:        require('./scale.json')
+  };
+});
